3c37ccceadd657076b90e4099932538caea403b8
Fan Jiang <fanjiang@thoughtworks.com>
2017-01-19 12:55:32 -0500

    minor change

--- mnist/expert.py
+++ mnist/expert.py
@@ -9,20 +9,25 @@ export_dir = './tmp/expert-export'
 if os.path.exists(export_dir):
     shutil.rmtree(export_dir)
 
+
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
+
 
 def bias_variable(shape):
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
 
+
 def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
 
 def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
                           strides=[1, 2, 2, 1], padding='SAME')
+
 
 mnist = input_data.read_data_sets("data/", one_hot=True)
 
@@ -63,7 +68,7 @@ with g.as_default():
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
 
-    for i in range(20000):
+    for i in range(3000):
         batch = mnist.train.next_batch(50)
         if i % 100 == 0:
             train_accuracy = accuracy.eval(
@@ -116,7 +121,7 @@ with g_2.as_default():
     y_conv_2 = tf.nn.softmax(tf.matmul(h_fc1_2, W_fc2_2) + b_fc2_2, name="output")
 
     sess_2 = tf.Session()
-    init_2 = tf.initialize_all_variables();
+    init_2 = tf.global_variables_initializer()
     sess_2.run(init_2)
 
     graph_def = g_2.as_graph_def()
