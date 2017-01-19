78205720ac8282ebee62ae9feb25327f8da6521b
Fan Jiang <fanjiang@thoughtworks.com>
2017-01-19 13:15:47 -0500

    update to latest api

--- mnist/expert.py
+++ mnist/expert.py
@@ -58,9 +58,10 @@ with g.as_default():
     W_fc2 = weight_variable([1024, 10])
     b_fc2 = bias_variable([10])
 
-    y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
-    cross_entropy = -tf.reduce_sum(y_ * tf.log(y_conv))
+    cross_entropy = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
     correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
@@ -68,7 +69,7 @@ with g.as_default():
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
 
-    for i in range(3000):
+    for i in range(20000):
         batch = mnist.train.next_batch(50)
         if i % 100 == 0:
             train_accuracy = accuracy.eval(
